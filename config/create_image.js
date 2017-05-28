0f17bbf3f2939e9df952e19143b07fc4eb52723f
Kanak Bagga <kanakbagga1@gmail.com>
2017-05-28 18:18:31 +0530

    Everything is working now

--- config/create_image.js
+++ config/create_image.js
@@ -13,8 +13,17 @@ exports.create_image =function(email,quote,option,callback){
 	
 	console.log("\n\noption:\t"+JSON.stringify(option));
 	
-	fs.writeFileSync('output.png', text2png(quote, {textColor: 'blue',font: '80px Futura',bgColor: 'white',padding: 130}));
-//	fs.writeFileSync('output.png', text2png(quote, option));
+//	fs.writeFileSync('output.png', text2png(quote, {textColor: 'blue',font: '80px Futura',bgColor: 'white',padding: 130}));
+	fs.writeFileSync('output.png', text2png(quote, 
+				{
+					font : option.font,
+					textColor : option.textColor,
+					bgColor : option.bgColor,
+					lineSpacing : parseInt(option.lineSpacing),
+					padding : parseInt(option.padding),
+					
+				}
+				));
 	
 	var mailOptions = {
 				from: "<bagga.kanak1@gmail.com>",
